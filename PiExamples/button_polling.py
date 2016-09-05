638f9847a2e58fab33b02c51e35db6f892cd3add
J.R. Leeman <kd5wxb@gmail.com>
2016-09-04 21:06:56 -0400

    Change polling logic

--- PiExamples/button_polling.py
+++ PiExamples/button_polling.py
@@ -8,7 +8,7 @@ GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(button_pin)
 
-    if input_state is False:
+    if input_state == False:
         print('Button Press Detected')
         time.sleep(0.2)
 
